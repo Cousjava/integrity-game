68cb7fc107e358c8fa6ccaff62204588f3be4009
Zach <zach.sproston1@gmail.com>
2019-04-02 18:30:57 +0100

    Testing CI to check it falls over when tests fail (Fixed so CI works)

--- core/src/test/java/test/ScreenManagerTest.java
+++ core/src/test/java/test/ScreenManagerTest.java
@@ -28,7 +28,7 @@ public class ScreenManagerTest {
     @Test
     public void testGameInfoScreen(){
         test.changeScreen(ScreenManager.Screens.GAME_INFO);
-        verify(game, times(1)).setScreen(refEq(new MainMenuScreen()));
+        verify(game, times(1)).setScreen(refEq(new GameInfoScreen()));
     }
 
     @Test
