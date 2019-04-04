d6bfba0f270c3bee71943d9085f7c9e307ace365
Jonathan Coustick <jonathan.coustick@payara.fish>
2019-04-04 16:45:33 +0100

    An attempt at stuff. To delete probably

--- core/src/com/github/integritygame/TanksGame.java
+++ core/src/com/github/integritygame/TanksGame.java
@@ -1,28 +1,33 @@
 package com.github.integritygame;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.integrity.game.MainMenu;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
+public class TanksGame extends Game {
+	private SpriteBatch batch;
 	Texture img;
-	
+        	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+                this.setScreen(new MainMenu(this));
+                
+                
+                
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		/*Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
-		batch.end();
+		batch.end();*/
 	}
 	
 	@Override
@@ -30,4 +35,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+        
+        public SpriteBatch getSpriteBatch() {
+            return batch;
+        }
 }
--- desktop/src/com/github/integritygame/desktop/DesktopLauncher.java
+++ desktop/src/com/github/integritygame/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package com.github.integritygame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.github.integritygame.MyGdxGame;
+import com.github.integritygame.TanksGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+                config.height = 480;
+                config.width = 600;
+		new LwjglApplication(new TanksGame(), config);
 	}
 }
